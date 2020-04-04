60fa972b5df834a9eb2f94e5c6cafae8f517e9e9
MizuguchiRyuya <mizuguchi.ryuya.infratop@gmail.com>
2020-04-05 00:43:55 +0900

    create機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,28 @@ class BooksController < ApplicationController
 	end
 
 	def index
-		@books = Book.all
+		@books = Book.all.order('created_at DESC')
 		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		if @book.save
+			flash[:success] = 'successfully'
+			redirect_to book_path(@book.id)
+		else
+			@books = Book.all.order('created_at DESC')
+			render 'index'
+		end
 	end
 
 	def show
 		@book = Book.find(params[:id])
+	end
+
+	private
+
+	def book_params
+		params.require(:book).permit(:title, :body)
 	end
 end
